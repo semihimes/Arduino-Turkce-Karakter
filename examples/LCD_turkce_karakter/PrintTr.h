a8234a645dc67f642d923ed3a5a314c3d450f9b9
semihimes <58172073+semihimes@users.noreply.github.com>
2020-12-16 23:30:27 +0300

    Update PrintTr.h

--- examples/LCD_turkce_karakter/PrintTr.h
+++ examples/LCD_turkce_karakter/PrintTr.h
@@ -17,8 +17,8 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef Print_h
-#define Print_h
+#ifndef PrintTr_h
+#define PrintTr_h
 
 #include <inttypes.h>
 #include <stdio.h> // for size_t
